9ef00c38f3e64cc7255f4b5ff2745ec0210f66af
SagarDeokar <sgrdeokar@gmail.com>
2015-11-07 21:44:03 +0530

    First commit

---
+++ src/waitTypes.java
@@ -0,0 +1,86 @@
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class waitTypes {
+
+	// created reference variable for WebDriver
+
+	WebDriver drv;
+
+	@Before
+	public void setup() throws InterruptedException {
+
+		// initializing drv variable using FirefoxDriver
+
+		drv = new FirefoxDriver();
+
+		// launching gmail.com on the browser
+
+		drv.get("https://www.gmail.com");
+
+		// maximized the browser window
+
+		drv.manage().window().maximize();
+
+		drv.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+	}
+
+	@Test
+	public void test() throws InterruptedException {
+
+		// saving the GUI element reference into a "username" variable of
+		// WebElement type
+
+		WebElement username = drv.findElement(By.id("Email"));
+
+		// entering username
+
+		username.sendKeys("enter email id here");
+
+		// click on next button
+		WebElement next = drv.findElement(By.id("next"));
+		next.click();
+
+		// entering password
+
+		drv.findElement(By.id("Passwd")).sendKeys("enter password here");
+
+		// clicking signin button
+
+		drv.findElement(By.id("signIn")).click();
+
+		// explicit wait - to wait for the compose button to be click-able
+
+		WebDriverWait wait = new WebDriverWait(drv, 30);
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By
+				.xpath("//div[contains(text(),'COMPOSE')]")));
+
+		// click on the compose button as soon as the "compose" button is
+		// visible
+
+		drv.findElement(By.xpath("//div[contains(text(),'COMPOSE')]")).click();
+
+		Thread.sleep(3000);
+	}
+
+	@After
+	public void teardown() {
+
+		// closes all the browser windows opened by web driver
+
+		drv.quit();
+
+	}
+
+}
